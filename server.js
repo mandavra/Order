007e76d4a12a7695c0a9fa5f349ee9a718cb7a85
mandavra <141617041+mandavra@users.noreply.github.com>
2024-10-17 02:28:16 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { connectDB } = require("./db/dbConnect");
+const routes = require("./routes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { userService, orderService, menuService } = require("./services");
+const app = express();
+
+const options = {
+  origin: "*",
+};
+app.use(cors(options));
+
+//body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//cookies
+app.use(cookieParser());
+
+//routes
+app.use("/v1", routes);
+
+
+// public for img
+app.use(express.static('public'));
+
+
+//DB connect
+connectDB();
+
+//server
+http.createServer(app).listen(process.env.PORT, () => {
+  console.log("server started scuucess on 1310"); 
+});
